d34a44abb19d944721b22edf5de8bbbb1baf9cc6
User <User@LAB3-3>
2024-01-31 09:35:14 +0200

    Add project files.

---
+++ LoginPage/Service/UserService.cs
@@ -0,0 +1,33 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LoginPage.Service
+{
+
+    public class UserService
+    {
+        //רשימה של יוזרס חדשים
+        public List<Users> listUsers { get; set; }
+
+        public UserService()
+        {
+            this.listUsers = new List<Users>();
+            FillList();
+        }
+
+        //מכניס רשומים לרשימה
+        private void FillList()
+        {
+            listUsers.Add(new Users() { Name = "Gal", Password = "GalHatumtum" });
+            listUsers.Add( new Users() { Name = "Tami", Password="ShaharHatumtum"});
+            listUsers.Add( new Users() { Name = "Shahar", Password="FluffyHatumtum"});
+        }
+
+
+    }
+}
---
+++ LoginPage/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MonkeysMVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage.ViewModels
+{
+    public class LoginPageViewModel: ViewModel
+    {
+        private string name;
+        private string password;
+        public string Name { get { return name; } set { name = value; OnPropertyChanged(); } }
+        public string Password { get { return password; } set { password = value; OnPropertyChanged(); } }
+    }
+}
